996e1687ee5de9a3ee9e54a70003ca5f2c19848e
PatrickL546 <75874561+PatrickL546@users.noreply.github.com>
2024-04-22 12:57:56 -0400

    Update Hydrax-Abyss.to-DownloadHelper.user.js

--- Hydrax-Abyss.to-DownloadHelper.user.js
+++ Hydrax-Abyss.to-DownloadHelper.user.js
@@ -1,1 +1,37 @@
+// ==UserScript==
+// @name         Hydrax/Abyss.to DownloadHelper
+// @namespace    https://github.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper
+// @version      0.1
+// @description  Get Vid_ID
+// @icon64       https://raw.githubusercontent.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper/master/icon.png
+// @grant        GM_registerMenuCommand
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @author       PatrickL546
+// @match        *://*/*
+// @updateURL    https://github.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper/raw/master/Hydrax-Abyss.to-DownloadHelper.user.js
+// @downloadURL  https://github.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper/raw/master/Hydrax-Abyss.to-DownloadHelper.user.js
+// @supportURL   https://github.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper/issues
+// ==/UserScript==
 
+(function () {
+    'use strict';
+
+    const urlRe = /\?v=(.*?)$/;
+    const getVidIDUrl = [];
+
+    if (urlRe.exec(window.location.href)) {
+        getVidIDUrl.push(urlRe.exec(window.location.href)[1]);
+    };
+
+    if (getVidIDUrl.length) {
+        GM_setValue("vidID", Array.from(new Set(getVidIDUrl)));
+        GM_registerMenuCommand("Get Vid_ID", getVidID, "G");
+    };
+
+    function getVidID() {
+        const vidID = GM_getValue("vidID", null);
+        alert(`Copy Vid_ID: ${vidID}`);
+        console.log(`Copy Vid_ID: ${vidID[0]}`);
+    };
+})();
