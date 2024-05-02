46bd1c4447cb9ba9f7662db1b78297a0000086f7
PatrickL546 <75874561+PatrickL546@users.noreply.github.com>
2024-05-02 14:04:42 -0400

    Added referer and domain in alert

--- Hydrax-Abyss.to-DownloadHelper.user.js
+++ Hydrax-Abyss.to-DownloadHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hydrax/Abyss.to DownloadHelper
 // @namespace    https://github.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper
-// @version      0.2
+// @version      0.3
 // @description  Get Vid_ID
 // @icon64       https://raw.githubusercontent.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper/master/icon.png
 // @grant        GM_registerMenuCommand
@@ -20,21 +20,31 @@
     const urlRe = /\?v=([0-9A-Za-z_-]*)/;
     const atobRe = /PLAYER\(atob\("(.*)"\)\)/;
     const getVidIDUrl = [];
+    const getArrayAtobElement = [];
 
     document.querySelectorAll("script").forEach(element => {
         if (urlRe.exec(window.location.href) && (atobRe.exec(element.textContent))) {
             getVidIDUrl.push(urlRe.exec(window.location.href)[1]);
+            getArrayAtobElement.push(atobRe.exec(element.textContent)[1]);
         };
     });
 
     if (getVidIDUrl.length) {
         GM_setValue("vidID", Array.from(new Set(getVidIDUrl)));
+        GM_setValue("arrayAtobElement", Array.from(new Set(getArrayAtobElement)));
         GM_registerMenuCommand("Get Vid_ID", getVidID, "G");
     };
 
     function getVidID() {
         const vidID = GM_getValue("vidID", null);
-        alert(`Copy Vid_ID: ${vidID}`);
-        console.log(`Copy Vid_ID: ${vidID[0]}`);
+        const arrayAtobElement = GM_getValue("arrayAtobElement", null);
+
+        for (const atobElement of arrayAtobElement) {
+            const json = JSON.parse(atob(atobElement));
+            const jsonID = json.id;
+            const jsonDomain = json.domain;
+
+            alert(`Copy Vid_ID: ${vidID}                                                                 Referer: https://abysscdn.com/?v=${vidID}                                   Domain: ${jsonDomain}/${jsonID}`);
+        };
     };
 })();
