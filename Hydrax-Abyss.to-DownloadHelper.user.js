b5888cee3ac8a44388f23ec496abcdf65bb064cc
PatrickL546 <75874561+PatrickL546@users.noreply.github.com>
2024-05-04 21:32:44 -0400

    Show Vid_ID of current page instead of newest page loaded

--- Hydrax-Abyss.to-DownloadHelper.user.js
+++ Hydrax-Abyss.to-DownloadHelper.user.js
@@ -1,12 +1,10 @@
 // ==UserScript==
 // @name         Hydrax/Abyss.to DownloadHelper
 // @namespace    https://github.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper
-// @version      0.3
+// @version      0.4
 // @description  Get Vid_ID
 // @icon64       https://raw.githubusercontent.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper/master/icon.png
 // @grant        GM_registerMenuCommand
-// @grant        GM_setValue
-// @grant        GM_getValue
 // @author       PatrickL546
 // @match        *://*/*
 // @updateURL    https://github.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper/raw/master/Hydrax-Abyss.to-DownloadHelper.user.js
@@ -19,32 +17,24 @@
 
     const urlRe = /\?v=([0-9A-Za-z_-]*)/;
     const atobRe = /PLAYER\(atob\("(.*)"\)\)/;
-    const getVidIDUrl = [];
-    const getArrayAtobElement = [];
+    const vidIDUrl = [];
+    const arrayAtobElement = [];
 
     document.querySelectorAll("script").forEach(element => {
         if (urlRe.exec(window.location.href) && (atobRe.exec(element.textContent))) {
-            getVidIDUrl.push(urlRe.exec(window.location.href)[1]);
-            getArrayAtobElement.push(atobRe.exec(element.textContent)[1]);
+            vidIDUrl.push(urlRe.exec(window.location.href)[1]);
+            arrayAtobElement.push(atobRe.exec(element.textContent)[1]);
+            GM_registerMenuCommand("Get Vid_ID", getVidID, "G");
         };
     });
 
-    if (getVidIDUrl.length) {
-        GM_setValue("vidID", Array.from(new Set(getVidIDUrl)));
-        GM_setValue("arrayAtobElement", Array.from(new Set(getArrayAtobElement)));
-        GM_registerMenuCommand("Get Vid_ID", getVidID, "G");
-    };
-
     function getVidID() {
-        const vidID = GM_getValue("vidID", null);
-        const arrayAtobElement = GM_getValue("arrayAtobElement", null);
-
         for (const atobElement of arrayAtobElement) {
             const json = JSON.parse(atob(atobElement));
             const jsonID = json.id;
             const jsonDomain = json.domain;
 
-            alert(`Copy Vid_ID: ${vidID}                                                                 Referer: https://abysscdn.com/?v=${vidID}                                   Domain: ${jsonDomain}/${jsonID}`);
+            alert(`Copy Vid_ID: ${vidIDUrl}                                                                 Referer: https://abysscdn.com/?v=${vidIDUrl}                                   Domain: ${jsonDomain}/${jsonID}`);
         };
     };
 })();
