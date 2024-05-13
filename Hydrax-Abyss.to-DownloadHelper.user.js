04f3378c6a3fdf523c79f2dafc413efa2d6b5ae2
PatrickL546 <75874561+PatrickL546@users.noreply.github.com>
2024-05-13 09:02:03 -0400

    Add progress percentage, switch 480,360 shortcut, log to console, adjust progress bar, adjust byte size calc

--- Hydrax-Abyss.to-DownloadHelper.user.js
+++ Hydrax-Abyss.to-DownloadHelper.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Hydrax/Abyss.to DownloadHelper
 // @namespace    https://github.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper
-// @version      1.5
+// @version      1.6
 // @description  Downloads Hydrax/Abyss.to videos
 // @icon64       https://raw.githubusercontent.com/PatrickL546/Hydrax-Abyss.to-DownloadHelper/master/icon.png
 // @grant        GM_registerMenuCommand
@@ -57,10 +57,10 @@
                 GM_registerMenuCommand('Download 720p', download720, 'W');
             };
             if (pieceJson.mHd) {
-                GM_registerMenuCommand('Download 480p', download480, 'A');
+                GM_registerMenuCommand('Download 480p', download480, 'N');
             };
             if (pieceJson.sd) {
-                GM_registerMenuCommand('Download 360p', download360, 'N');
+                GM_registerMenuCommand('Download 360p', download360, 'A');
             };
 
             GM_registerMenuCommand('Show Info', showInfo, 'S');
@@ -73,33 +73,40 @@
         const container = document.createElement('div');
         container.id = 'DownloadHelper-progress-container';
         container.style.backgroundColor = '#1F1F1F';
-        container.style.position = 'fixed';
         container.style.width = '100%';
+        container.style.position = 'fixed';
         container.style.zIndex = '100000000';
 
         const text = document.createElement('div');
-        text.className = 'jw-settings-content-item';
         text.id = 'DownloadHelper-progress-text';
         text.style.fontFamily = 'Arial, Helvetica, sans-serif';
         text.style.color = 'rgba(255, 255, 255, 0.8)';
-        text.style.textShadow = 'black 1px 1px 3px';
+        text.style.textShadow = 'black 0 0 0.3em';
+        text.style.fontSize = '4vh';
+        text.style.lineHeight = '5vh';
         text.style.textAlign = 'center';
+        text.style.width = '100%';
         text.style.position = 'fixed';
-        text.style.padding = '0';
+        text.style.cursor = 'pointer';
 
         const progressBar = document.createElement('div');
         progressBar.id = 'DownloadHelper-progress-bar';
         progressBar.style.backgroundColor = '#44D62C';
-        progressBar.style.height = '3vh';
         progressBar.style.width = '0';
+        progressBar.style.height = '5vh';
 
         const button = document.createElement('button');
-        button.className = 'jw-reset jw-settings-content-item';
+        button.id = 'DownloadHelper-cancel-button';
+        button.className = 'jw-settings-content-item';
         button.textContent = 'Cancel';
-        button.style.fontWeight = 'bold';
         button.style.backgroundColor = '#333333';
+        button.style.fontWeight = 'bold';
+        button.style.fontSize = '4vh'
+        button.style.width = '11vw';
+        button.style.height = '6vh';
+        button.style.padding = '0';
+        button.style.border = '0';
         button.style.position = 'fixed';
-        button.style.width = '69.3594px'
         button.onclick = () => { download.abort() };
 
         container.appendChild(text);
@@ -109,15 +116,14 @@
     };
 
     function getSize(size) {
-        let sizes = [' Bytes', ' KB', ' MB', ' GB',
-            ' TB', ' PB', ' EB', ' ZB', ' YB'];
+        const sizes = [' Bytes', ' KB', ' MB', ' GB', ' TB', ' PB', ' EB', ' ZB', ' YB', ' RB', ' QB'];
 
         for (let i = 1; i < sizes.length; i++) {
-            if (size < Math.pow(1024, i))
-                return (Math.round((size / Math.pow(
-                    1024, i - 1)) * 100) / 100).toFixed(2) + sizes[i - 1];
-        }
-        return size;
+            if (size < Math.pow(1024, i)) {
+                return (size / Math.pow(1024, i - 1)).toFixed(2) + sizes[i - 1];
+            };
+        };
+        return size + sizes[0];
     };
 
     function download(url, name) {
@@ -128,12 +134,13 @@
             onprogress: (progress) => {
                 const progressBar = document.getElementById('DownloadHelper-progress-bar');
                 const percent = (progress.loaded / progress.total) * 100;
-                const progressText = document.getElementById('DownloadHelper-progress-text');
-                progressText.textContent = (`${getSize(progress.loaded)} / ${getSize(progress.total)}`)
                 progressBar.style.width = `${percent}%`;
+
+                const progressText = document.getElementById('DownloadHelper-progress-text');
+                progressText.textContent = `(${percent.toFixed(2)}%) ${getSize(progress.loaded)} / ${getSize(progress.total)}`;
             },
             onerror: (err) => {
-                console.log(err);
+                console.warn(err);
                 alert(`Download error: ${err.error}\nReason: ${JSON.stringify(err.details)}`);
                 document.getElementById('DownloadHelper-progress-container').remove();
             },
@@ -147,24 +154,32 @@
     function download1080() {
         download(url1080, `${vidID}_1080.mp4`);
     };
+
     function download720() {
         download(url720, `${vidID}_720.mp4`);
     };
+
     function download480() {
         download(url480_360, `${vidID}_480.mp4`);
     };
+
     function download360() {
         download(url480_360, `${vidID}_360.mp4`);
     };
+
     function showInfo() {
         alert(info);
     };
+
     function copyInfo() {
         GM_setClipboard(info);
+        console.log(info);
         alert('Copied info to clipboard');
     };
+
     function copyVidID() {
         GM_setClipboard(vidID);
+        console.log(vidID);
         alert(`Copied "${vidID}" to clipboard`);
     };
 })();
